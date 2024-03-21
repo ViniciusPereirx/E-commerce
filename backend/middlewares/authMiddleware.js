286386f4ad2ac6a8aea7e2c1d8fbaff6f3fc106a
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2024-03-21 20:50:45 -0300

    E-commerce

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import asyncHandler from "./asyncHandler.js";
+
+const authenticate = asyncHandler(async (req, res, next) => {
+  let token;
+  token = req.cookies.jwt;
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Token não encontrado.");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Falha ao encontrar o token.");
+  }
+});
+
+const authorizeAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send("Você não é um Admin.");
+  }
+};
+
+export { authenticate, authorizeAdmin };
