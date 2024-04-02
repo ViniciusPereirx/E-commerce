4efbd0d7bece2bf80db62abf3b00069e4b41db05
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2024-04-01 23:34:46 -0300

    E-commerce

--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -2,13 +2,17 @@ import { apiSlice } from "./apiSlice";
 import { USERS_URL } from "../constants";
 
 export const userApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        login:builder.mutation({
-            query: (data) => ({
-                url: `${USERS_URL}/auth`,
-                method: 'POST',
-                body: data,
-            })
-        })
-    })
-})
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+  }),
+});
+
+``
+
+export const { useLoginMutation } = userApiSlice;
