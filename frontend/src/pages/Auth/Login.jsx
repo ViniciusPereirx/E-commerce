751ea0e797311e4016d50e97e0fc469582357056
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2024-04-02 19:57:24 -0300

    E-commerce

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,7 +1,109 @@
-
+import { useState, useEffect } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { setCredientials } from "../../redux/features/auth/authSlice";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const { search } = useLocation();
+  const sp = new URLSearchParams(search);
+  const redirect = sp.get("redirect") || "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect);
+    }
+  }, [navigate, redirect, userInfo]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await login({ email, password }).unwrap();
+      console.log(res);
+      dispatch(setCredientials({ ...res }));
+    } catch (error) {
+      toast.error(error?.data?.message || error.message);
+    }
+  };
+
+  return (
+    <div>
+      <section className="pl-[10rem] flex flex-wrap">
+        <div className="mr-[4rem] mt-[5rem]">
+          <h1 className="text-2xl font-semibold mb-4">Entrar</h1>
+
+          <form onSubmit={submitHandler} className="container w-[40rem]">
+            <div className="my-[2rem]">
+              <label
+                htmlFor="email"
+                className="block tex-sm font-medium text-white"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                className="mt-1 p-2 border rounded w-full"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="my-[2rem]">
+              <label
+                htmlFor="password"
+                className="block tex-sm font-medium text-white"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="mt-1 p-2 border rounded w-full"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <button
+              disabled={isLoading}
+              type="submit"
+              className="bg-green-500 text-black px-4 py-2 rounded cursor-pointer my-[1rem]"
+            >
+              {isLoading ? "Entrando..." : "Entrar"}
+            </button>
+
+            {isLoading && <Loader />}
+          </form>
+
+          <div className="mt-4">
+            <p className="text-white">
+              New Custumer ? {""}
+              <Link
+                to={redirect ? `/register?redirect=${redirect}` : "/register"}
+                className="text-red-500 hover:underline"
+              >
+                Registra-se
+              </Link>
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Login;
