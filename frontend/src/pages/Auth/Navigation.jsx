0875b4daa9491ed3a9067a61a0cd123cfafec2af
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2024-04-03 00:11:50 -0300

    E-commerce

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -11,7 +11,7 @@ import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { useLoginMutation } from "../../redux/api//usersApiSlice";
+import { useLogoutMutation } from "../../redux/api//usersApiSlice";
 import { logout } from "../../redux/features/auth/authSlice";
 
 import "./Navigation.css";
@@ -37,7 +37,7 @@ const Navigation = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const [logoutApiCall] = useLoginMutation();
+  const [logoutApiCall] = useLogoutMutation();
 
   const logoutHandler = async () => {
     try {
@@ -93,32 +93,126 @@ const Navigation = () => {
           onClick={toggleDropdown}
           className="flex items-center text-gray-8000 focus:outline-none"
         >
-          {userInfo ? <span className="text-white">{userInfo.username}</span> : (<></>)}
+          {userInfo ? (
+            <span className="text-white">{userInfo.username}</span>
+          ) : (
+            <></>
+          )}
+
+          {userInfo && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={`h-4 w-4 ml-1 ${
+                dropdownOpen ? "transform rotate-180" : ""
+              }`}
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="white"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d={dropdownOpen ? "M5 15l7-7 7 7" : "M19 9l-7 7-7-7"}
+              />
+            </svg>
+          )}
         </button>
+
+        {dropdownOpen && userInfo && (
+          <ul
+            className={`absolute right-0 mt-2 mr-11 space-y-2 bg-white text-gray-600 ${
+              !userInfo.isAdmin ? "-top-20" : "-top-80"
+            }`}
+          >
+            {userInfo.isAdmin && (
+              <>
+                <li>
+                  <Link
+                    to={"/admin/dashboard"}
+                    className="block px-4 py-2 hover:bg-gray-400 text-black"
+                  >
+                    Dashboard
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/admin/productlist"}
+                    className="block px-4 py-2 hover:bg-gray-400 text-black"
+                  >
+                    Produtos
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/admin/categorylist"}
+                    className="block px-4 py-2 hover:bg-gray-400 text-black"
+                  >
+                    Categorias
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/admin/orderlist"}
+                    className="block px-4 py-2 hover:bg-gray-400 text-black"
+                  >
+                    Pedidos
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to={"/admin/userlist"}
+                    className="block px-4 py-2 hover:bg-gray-400 text-black"
+                  >
+                    Usuários
+                  </Link>
+                </li>
+              </>
+            )}
+            <li>
+              <Link
+                to={"/admin/profile"}
+                className="block px-4 py-2 hover:bg-gray-400 text-black"
+              >
+                Perfil
+              </Link>
+            </li>
+            <li>
+              <Link
+                onClick={logoutHandler}
+                className="block px-4 py-2 hover:bg-gray-400 text-black"
+              >
+                Sair
+              </Link>
+            </li>
+          </ul>
+        )}
       </div>
 
-      <ul>
-        <li>
-          <Link
-            to="/login"
-            className="flex items-center transition-transform transform hover:translate-x-2"
-          >
-            <AiOutlineLogin className="mr-2 mt-[3rem]" size={26} />
-            <span className="hidden nav-item-name mt-[3rem]">Login</span>{" "}
-          </Link>
-        </li>
-        <li>
-          <Link
-            to="/register"
-            className="flex items-center transition-transform transform hover:translate-x-2"
-          >
-            <AiOutlineUserAdd className="mr-2 mt-[3rem]" size={26} />
-            <span className="hidden nav-item-name mt-[3rem]">
-              Register
-            </span>{" "}
-          </Link>
-        </li>
-      </ul>
+      {!userInfo && (
+        <ul>
+          <li>
+            <Link
+              to="/login"
+              className="flex items-center transition-transform transform hover:translate-x-2"
+            >
+              <AiOutlineLogin className="mr-2 mt-[3rem]" size={26} />
+              <span className="hidden nav-item-name mt-[3rem]">Login</span>{" "}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/register"
+              className="flex items-center transition-transform transform hover:translate-x-2"
+            >
+              <AiOutlineUserAdd className="mr-2 mt-[3rem]" size={26} />
+              <span className="hidden nav-item-name mt-[3rem]">
+                Register
+              </span>{" "}
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
