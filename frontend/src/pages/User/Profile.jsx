3bfd5567b34f37d093054da3e67f7632c9b1fbce
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2024-04-22 19:37:37 -0300

    E-commerce

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import Loader from "../../components/Loader";
+import { setCredientials } from "../../redux/features/auth/authSlice";
+import { Link } from "react-router-dom";
+import useProfileMutation from "../../redux/api/usersApiSlice";
 
 function Profile() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const [updateProfile, { isLoading: loadingUpdateProfile }] =
+    useProfileMutation();
+
+  useEffect(() => {
+    setUsername(userInfo.username);
+    setEmail(userInfo.email);
+  }, [userInfo.email, userInfo.username]);
+
+  const dispatch = useDispatch();
+
+
   return <div>Profile</div>;
 }
 
--- frontend/src/redux/api/usersApiSlice.js
+++ frontend/src/redux/api/usersApiSlice.js
@@ -19,15 +19,28 @@ export const userApiSlice = apiSlice.injectEndpoints({
     }),
 
     register: builder.mutation({
-      query: data => ({
+      query: (data) => ({
         url: `${USERS_URL}`,
-        method: 'POST',
+        method: "POST",
         body: data,
-      })
-    })
+      }),
+    }),
+
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: "PUT",
+        body: data,
+      }),
+    }),
   }),
 });
 
 ``;
 
-export const { useLoginMutation, useLogoutMutation, useRegisterMutation } = userApiSlice;
+export const {
+  useLoginMutation,
+  useLogoutMutation,
+  useRegisterMutation,
+  useProfileMutation,
+} = userApiSlice;
