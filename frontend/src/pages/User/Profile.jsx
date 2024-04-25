7f89f86573d77b3e6116e6cda2bc87fb0c5fd6f3
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2024-04-25 20:56:01 -0300

    E-commerce

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -4,10 +4,10 @@ import { toast } from "react-toastify";
 import Loader from "../../components/Loader";
 import { setCredientials } from "../../redux/features/auth/authSlice";
 import { Link } from "react-router-dom";
-import useProfileMutation from "../../redux/api/usersApiSlice";
+import { useProfileMutation } from "../../redux/api/usersApiSlice";
 
 function Profile() {
-  const [username, setUsername] = useState("");
+  const [username, setUserName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
@@ -18,14 +18,102 @@ function Profile() {
     useProfileMutation();
 
   useEffect(() => {
-    setUsername(userInfo.username);
+    setUserName(userInfo.username);
     setEmail(userInfo.email);
   }, [userInfo.email, userInfo.username]);
 
   const dispatch = useDispatch();
 
+  const submitHandler = async (e) => {
+    e.preventDefault();
 
-  return <div>Profile</div>;
+    if (password !== confirmPassword) {
+      toast.error("Senhas não estão combinando.");
+    } else {
+      try {
+        const res = await updateProfile({
+          _id: userInfo._id,
+          username,
+          email,
+          password,
+        }).unwrap();
+        dispatch(setCredientials({...res}));
+        toast.success('Perfil atualizado com sucesso.')
+      } catch (error) {
+        toast.error(error?.data?.message || error.message);
+      }
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-4 mt-[6rem]">
+      <div className="flex justify-center align-center md:flex md:space-x-4">
+        <div className="md:w-1/3">
+          <h2 className="text font-semibold mb-4">Atualizar Perfil</h2>
+          <form onSubmit={submitHandler}>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Nome</label>
+              <input
+                type="text"
+                placeholder="Informe seu nome"
+                className="form-input p-4 rounded-sm w-full"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Email</label>
+              <input
+                type="email"
+                placeholder="Informe seu email"
+                className="form-input p-4 rounded-sm w-full"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Senha</label>
+              <input
+                type="password"
+                placeholder="Informe sua senha"
+                className="form-input p-4 rounded-sm w-full"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block text-white mb-2">Confirmar senha</label>
+              <input
+                type="password"
+                placeholder="Confirme sua senha"
+                className="form-input p-4 rounded-sm w-full"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="flex justify-between">
+              <button
+                type="submit"
+                className="bg-green-500 py-2 px-4 rounded hover:bg-green-700"
+              >
+                Atualizar
+              </button>
+
+              <Link
+                to={"/user-orders"}
+                className="bg-green-700 text-white py-2 px-4 rounded hover:bg-green-900"
+              >
+                Meus pedidos
+              </Link>
+            </div>
+          </form>
+        </div>
+
+        {loadingUpdateProfile && <Loader />}
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
